e8167f1515b1bd93d524f584b741b9d856689b00
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-18 11:02:30 +0530

    sended req from postman to server

--- src/app.js
+++ src/app.js
@@ -2,12 +2,19 @@ const express=require('express');
 
 const app=express();
 
+app.use(express.json())
 // task-notes
 
 const notes=[];
 
-app.post("/notes",()=>{
-    
+app.post("/notes",(req,res)=>{
+notes.push(req.body);
+
+res.status(201).json({
+    message:"note created successfully"
 })
+
+})
+
 
 module.exports=app;
