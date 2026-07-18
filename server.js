b6edb309be47e562f23682b11cb51a0cb2d37ab0
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-18 10:36:24 +0530

    started with the notes task

--- server.js
+++ server.js
@@ -3,3 +3,5 @@ const app=require("./src/app");
 app.listen(3000,()=>{
     console.log("The server is running on port 3000");
 })
+
+
--- src/app.js
+++ src/app.js
@@ -2,4 +2,12 @@ const express=require('express');
 
 const app=express();
 
+// task-notes
+
+const notes=[];
+
+app.post("/notes",()=>{
+    
+})
+
 module.exports=app;
